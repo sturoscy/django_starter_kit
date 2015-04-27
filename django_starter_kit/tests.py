ce44e5c7118e85aadf6f25a9225f85f98ba2b0fb
ozymandias <moza@wharton.upenn.edu>
2015-04-27 16:47:49 -0400

    cleaned

--- django_starter_kit/tests.py
+++ django_starter_kit/tests.py
@@ -17,18 +17,3 @@ class HomePageTest(TestCase):
         response = self.client.get('', follow=True)
         self.assertEqual(response.status_code, 200)
 
-
-        
-    # def setUp(self):
-    #     # Every test needs a client.
-    #     self.client = Client()
-
-    # def test_details(self):
-    #     # Issue a GET request.
-    #     response = self.client.get('/customer/details/')
-
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-
-    #     # Check that the rendered context contains 5 customers.
-    #     self.assertEqual(len(response.context['customers']), 5)
