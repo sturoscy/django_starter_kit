f63bf7b0603e254b964481aa9a4c05d558e30cf1
Turoscy, Steve <sturoscy@wharton.upenn.edu>
2015-06-04 14:32:48 -0400

    Cleared git cached

--- django_starter_kit/reset_wsgi.py
+++
@@ -1,5 +0,0 @@
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '': 
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
--- django_starter_kit/settings/vagrant.py
+++
@@ -1,8 +0,0 @@
-from .dev import *
-
-ROLLBAR = {
-    'access_token': 'YOUR_POST_SERVER_ITEM_ACCESS_TOKEN',
-    'environment': 'development',
-    'branch': 'develop',
-    'root': '/vagrant/html/<app>',
-}
